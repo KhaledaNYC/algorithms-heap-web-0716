8ba5ec9af038224c9b63381d1968fa34a34a5c93
Khaleda Khan <KhaledaNYC@Khaledas-MacBook.local>
2016-11-02 17:38:40 -0400

    too easy? too good to be true? heaps

--- lib/algorithms_heap.rb
+++ lib/algorithms_heap.rb
@@ -1,1 +1,19 @@
 # code your solution here
+class MinHeap
+  attr_accessor :data, :root
+  def initialize
+    @data = []
+  end
+
+  def push(val)
+    @data << val
+    @root = @data.sort[0]
+  end
+
+  def pop
+    last_el = @data.pop
+    @root = @data.sort[0]
+    last_el
+  end
+
+end
